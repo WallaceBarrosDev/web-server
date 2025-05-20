cfd776ca552be715b8b210e4538a3f3718df6848
Wallace Barros da Cruz <w.wallacebarro.cruz@gmail.com>
2025-05-20 01:03:02 +0000

    feat: adicionando .env

--- app/src/server.ts
+++ app/src/server.ts
@@ -1,10 +1,14 @@
 import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
 const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (_req: Request, res: Response) => {
     res.json({message: 'Hello World!'});
 })
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
